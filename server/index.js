da0f83f34f8d4873e4c2fb797b2b2ba1b4440339
IsmailKoormath <ismailkoormath037@gmail.com>
2024-12-19 12:10:39 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import ip from "ip";
+import "dotenv/config";
+import { errorHandling } from "./middlewares/error.middleware.js";
+import { dbConnection } from "./config/dbConnection.js";
+
+const app = express();
+
+app.use(express.json());
+
+await dbConnection();
+
+app.use(cors({ origin: true, credentials: true }));
+
+// app.use("/api/auth", authRoute);
+// app.use("/api/user", userRoute);
+
+app.use(errorHandling);
+
+app.get("/", (req, res) => {
+  res.status(201).json("Running");
+});
+
+const port = 1001;
+app.listen(port, () => {
+  console.log(`App listening on the port ${ip.address()}:${port}`);
+});
